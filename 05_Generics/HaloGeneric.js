6b4a8650f04dba95e4e699971dbe577e85f4aaca
Dhiya Ulhaq Ramadhan <dhiyau677@gmail.com>
2025-04-08 22:29:34 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/HaloGeneric.js
@@ -0,0 +1,11 @@
+class HaloGeneric {
+    SapaUser(x) {
+      console.log(`Halo user ${x}`);
+    }
+  }
+  
+  const halo = new HaloGeneric();
+  
+  halo.SapaUser("Dhiya");
+  
+  console.log("=== Code Execution Successful ===");
