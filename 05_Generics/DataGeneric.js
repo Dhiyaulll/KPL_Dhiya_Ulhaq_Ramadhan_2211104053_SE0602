4b7a610326ac85081783946cab0eba3efc079b08
Dhiya Ulhaq Ramadhan <dhiyau677@gmail.com>
2025-04-08 23:04:04 +0700

    menambahkan class DataGeneric

---
+++ 05_Generics/DataGeneric.js
@@ -0,0 +1,13 @@
+class DataGeneric {
+    constructor(data) {
+        this.data = data;
+    }
+    
+    PrintData() {
+        console.log(`Data yang tersimpan adalah: ${this.data}`);
+    }
+}
+
+const dataGeneric = new DataGeneric("Dhiya Ulhaq Ramadhan dengan nim : 2211104053");
+dataGeneric.PrintData();
+console.log("=== Code Execution Successful ===");
